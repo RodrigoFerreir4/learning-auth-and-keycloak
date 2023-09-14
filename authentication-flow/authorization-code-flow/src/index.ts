ada546939a78dfa0742e26415bcbcd2b9369c7c5
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-09-13 21:34:36 -0400

    feate: exchanging authorization code for access token

--- authentication-flow/authorization-code-flow/src/index.ts
+++ authentication-flow/authorization-code-flow/src/index.ts
@@ -15,6 +15,32 @@ app.get("/login", (req, res) => {
   res.redirect(url);
 });
 
+app.get("/callback", async (req, res) => {
+  console.log(req.query);
+
+  const bodyParams = new URLSearchParams({
+    grant_type: "authorization_code",
+    client_id: "test-client",
+    code: req.query.code as string,
+    redirect_uri: "http://localhost:3000/callback",
+  });
+  const url = `http://keycloak:8080/realms/realm-for-test/protocol/openid-connect/token`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: bodyParams.toString(),
+  });
+
+  const result = await response.json();
+  console.log(result);
+  res.json(result);
+
+  res.json;
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
